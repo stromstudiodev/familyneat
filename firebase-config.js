d83e6f51671d99c691d89a4897cc860325282bb8
stromstudiodev <stromstudiodev@gmail.com>
2026-08-31 21:25:21 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCSSiNMm9GCPxbf_RXkVXazM9cU5qQ-tVw",
+  authDomain: "familyneat-d8771.firebaseapp.com",
+  projectId: "familyneat-d8771",
+  storageBucket: "familyneat-d8771.firebasestorage.app",
+  messagingSenderId: "369824178767",
+  appId: "1:369824178767:web:66a9fc0c2a680eb4db803a",
+};
